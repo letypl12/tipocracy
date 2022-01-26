01e4e446d103d75b9b5bc3d42a2d51b88efbe92b
exitexit <eunicegore@gmail.com>
2022-01-26 12:44:18 -0800

    tab navigation

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View, Button } from "react-native";
 // import * as React from 'react';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 function HomeScreen({ navigation }) {
   return (
@@ -25,20 +26,25 @@ function TeamsScreen() {
   );
 }
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 // export default function App() {
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Teams" component={TeamsScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Teams" component={TeamsScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
 export default App;
+// const Stack = createNativeStackNavigator();
+// <Stack.Navigator initialRouteName="Home">
+//         <Stack.Screen name="Home" component={HomeScreen} />
+//         <Stack.Screen name="Teams" component={TeamsScreen} />
+//       </Stack.Navigator>
 
 const styles = StyleSheet.create({
   container: {
