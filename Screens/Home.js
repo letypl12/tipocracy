3060590ae0b21ab99f932f33b26a03f278ef0c9e
exitexit <eunicegore@gmail.com>
2022-01-31 16:22:44 -0800

    display current user on home screen

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,21 +1,22 @@
-import React, { useEffect, useState, useContext, useReducer } from 'react';
+import React, { useEffect, useState, useContext, useReducer } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
-import { AuthContext } from '../utils/authContext';
-
-
+import { AuthContext } from "../utils/authContext";
+import auth, { firebase } from "@react-native-firebase/auth";
+import firestore from "@react-native-firebase/firestore";
 
 function HomeScreen({ navigation }) {
-  
-  const {signOut} = useContext(AuthContext);
+  const { signOut } = useContext(AuthContext);
 
   const logout = () => {
     console.log("in logout");
     signOut();
   };
 
+  let user = firebase.auth().currentUser.displayName;
+
   return (
     <View style={styles.container}>
-      <Text style={styles.setFontSizeOne}>Hello User!:</Text>
+      <Text style={styles.setFontSizeOne}>Hello {user}!:</Text>
 
       <Button
         title="Want to join a Team?"
