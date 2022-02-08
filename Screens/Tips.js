3a67869fe1121e9e6fb8fa86696880ff857328c4
exitexit <eunicegore@gmail.com>
2022-02-07 23:53:04 -0800

    create a tip, Validation of tips, returning to home and playing with render function for tips

--- Screens/Tips.js
+++ Screens/Tips.js
@@ -1,47 +1,133 @@
 import React from "react";
 import { useEffect, useState, useContext } from "react";
-import { StyleSheet, Text, View,  TextInput } from "react-native";
-import {Input, Button} from "react-native-elements";
-import styles  from "../utils/styles";
-
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import { Input, Button, ActivityIndicator } from "react-native-elements";
+import styles from "../utils/styles";
+// import { firebase } from "@react-native-firebase/auth";
+import { firebase, firestore } from "@react-native-firebase/firestore";
+import { validateAll, validations, validate } from "indicative/validator";
 
 function TipsScreen({ navigation }) {
   const [TipErrors, setTipErrors] = useState({});
-  const [tip, setTip] = React.useState(0.00);
+  const [tip, setTip] = React.useState(0.0);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const saveTip = () =>{
+  const saveTip = () => {
     // validate the input
+    const rules = {
+      tip: "required|number|above:0",
+      // tip: [
+      //   validations.number(),
+      //   validations.above([0]),
+      //   validations.required(),
+      // ],
+    };
 
+    const data = {
+      tip: tip,
+    };
 
-    // create a new firestore record, set isLoading to true
-    let timestamp = firebase.firestore.FieldValue.serverTimestamp();
-    firestore()
-    .collection("Tips")
-    .add({
-      team_uid: global.userToken.defaultTeam,
-      uid: global.userToken.uid,
-      amount: tip,
-      createdDateTime: timestamp,
-    })   
-    //then set isLoading to false and send them home
-    
-  }
+    const messages = {
+      required: (field) => `${field} is required`,
+      "tip.required": "An amount is required to save a tip",
+      "tip.number": "Make sure to enter a number",
+      "tip.above": "The tip should be greater than 0",
+    };
+
+    validateAll(data, rules, messages)
+      .then(async () => {
+        let timestamp = firebase.firestore.FieldValue.serverTimestamp();
+        firebase
+          .firestore()
+          .collection("Tips")
+          .add({
+            team_uid: global.userToken.defaultTeam,
+            uid: global.userToken.uid,
+            amount: tip,
+            createdDateTime: timestamp,
+          })
+          .then(() => {
+            console.log("tip added");
+
+            navigation.navigate("Home", { reload: true });
+            setTip(0.0);
+          });
+      })
+      // create a new firestore record, set isLoading to true
+
+      //then set isLoading to false and send them home
+
+      .catch((err) => {
+        console.log("caught:" + JSON.stringify(err));
+        const formatError = {};
+        for (let myErr of err) {
+          formatError[myErr.field] = myErr.message;
+        }
+        setTipErrors(formatError);
+      });
+  };
 
   //function to render the page components based on if a team is chosen
+  // const renderTips = () => {
+  //   chosenTeam = global.userToken.defaultTeam;
+  //   console.log("in tips with default team: " + chosenTeam);
 
-//check if isLoading and return ActivityIndicator
+  //   if (chosenTeam != global.team_uid) {
+  //     return (
+  //       <View style={styles.container}>
+  //         <Text>Choose a team: </Text>
+  //       </View>
+  //     );
+  //   } else {
+  //     return (
+  //       <View style={styles.container}>
+  //         <Text>You are on Team: {chosenTeam} </Text>
+  //         <Text>Enter Tip:</Text>
 
+  //         <Input
+  //           label={"Tip"}
+  //           placeholder="0.00"
+  //           value={tip}
+  //           onChangeText={setTip}
+  //           errorStyle={{ color: "red" }}
+  //           errorMessage={TipErrors ? TipErrors.tip : null}
+  //           keyboardType="decimal-pad"
+  //         />
+  //         <Button title="Save" onPress={saveTip}></Button>
+  //       </View>
+  //     );
+  //   }
+  // };
 
-//otherwise show the screen
+  // chosenTeam = global.userToken.defaultTeam;
+  // const renderTips = (chosenTeam) => {
+  //   if (chosenTeam) {
+  //     console.log("rendering tips");
+  //     return (
+  //       <View style={styles.container}>
+  //         <Text>Select a Team: {chosenTeam} </Text>
+  //         <Text>Enter Tip:</Text>
+  //       </View>
+  //     );
+  //   }
+  // };
+
+  //check if isLoading and return ActivityIndicator
+  if (!isLoading) {
+    return <ActivityIndicator />;
+  }
+
+  //otherwise show the screen
 
   return (
     <View style={styles.container}>
-      
+      {/* {renderTips()} */}
+
       {/*  
       create a render function and then show a message to choose a team if defaultTeam and global.teamName are not set
       otherwise show the form to add a tip
       */}
-      
+
       <Text>You are on Team: {global.teamName} </Text>
       <Text>Enter Tip:</Text>
 
@@ -53,9 +139,8 @@ function TipsScreen({ navigation }) {
         errorStyle={{ color: "red" }}
         errorMessage={TipErrors ? TipErrors.tip : null}
         keyboardType="decimal-pad"
-      />      
+      />
       <Button title="Save" onPress={saveTip}></Button>
-      
     </View>
   );
 }
