fe951f683525e0bf5a51fbc0e9f150ffd240afdb
exitexit <eunicegore@gmail.com>
2022-02-14 14:53:01 -0800

    Getting tips by user filtered by date range

--- Screens/History.js
+++ Screens/History.js
@@ -1,12 +1,14 @@
 import {useEffect, useState} from "react";
 import React from "react";
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import { StyleSheet, Text, View, TextInput, FlatList } from "react-native";
 
 // import { navigation } from '@react-navigation/native';
 import { Button, CheckBox, Icon, Card, Input } from "react-native-elements";
 import DatePicker from 'react-native-date-picker';
 import {moment} from 'moment';
 import { ScrollView } from "react-native-gesture-handler";
+import styles from "../utils/styles";
+import firestore from "@react-native-firebase/firestore";
 
 
 function HistoryScreen({ navigation }) {
@@ -18,12 +20,70 @@ function HistoryScreen({ navigation }) {
 //Refresh the screen on navigation listener thing
 
 //function to pull the records between the dates
+const getTipsRangeDate = async (queryparam, queryvalue) => {
+  // firebase
+  // if (queryvalue == ''){
+  //   return false
+  // }
+    let myTips = [];
+    // let date24 = moment().subtract(24, "hours").toDate();
+    console.log(startDate);
+    console.log(firestore.Timestamp.fromDate(startDate));
+    firestore()
+    .collection("Tips")
+    .where('createdDateTime', ">=", startDate)
+    .where("createdDateTime", "<=", endDate)
+    .get()
+    .then((querySnapshot) => {
+      console.log("Firestore Total tips records: " + querySnapshot.size + " for " + queryparam + " : " + queryvalue + startDate + endDate);
+     
+      querySnapshot.forEach((documentSnapshot) => {
+        let tmpTip = {
+                        amount: documentSnapshot.get('amount'),
+                        createdDateTime: documentSnapshot.get("createdDateTime"),
+                        team_uid: documentSnapshot.get("team_uid"),
+                        uid: documentSnapshot.get("uid")
+                      }
+        myTips.push(tmpTip)                        
+       
+      });
+      for (i=0; i<myTips.length; i++){
+        // myTips[i].amount = parseFloat(myTips[i].amount);
+        myTips[i].amount = parseFloat(myTips[i].amount);
+        myTips[i].createdDateTime=(myTips[i].createdDateTime).toDate()
+
+      }
+      console.log('Length of new tips object:' + myTips.length);   
+      console.log('TIPS: ' + JSON.stringify(myTips));
+
+      // teamTipsTotal=myTips.reduce((total, currentValue) => total = total + (currentValue.amount),0);
+    
+      // result = myTips.reduce((total, currentValue) => total = total + (currentValue.amount),0);
+      // console.log('in getTipsTotal, with query for: ' + queryparam + ': ' + queryvalue + ', the total is: ' + result);
+      // if (queryparam == 'uid'){
+      //   setMyTipsTotal(parseFloat(result).toFixed(2))
+      // }else{
+      //   setTeamTipsTotal(parseFloat(result).toFixed(2));
+      // }
+    }); 
+
+      
+
+}
+
 
 
 //function to render the tip records
- const renderTipRecords = () =>{
+//  const renderTipRecords = () =>{
+  const Item = ({ title }) => (
+    <View style={styles.item}>
+      <Text style={styles.item_myTips}>{title}</Text>
+    </View>
+  );
+  const renderTipRecords = ({ Item }) => <Item title={item.amount} />;
 
- }
+
+//  }
 
   return (
     <View style={{ flex: 1, flexDirection:'column', alignItems:'flex-start', alignContent:'flex-start' }}>
@@ -70,11 +130,26 @@ function HistoryScreen({ navigation }) {
           </View>
         </View>
         <View style={{ flex: 1, flexDirection:'row', alignItems: "center", justifyContent: "center" }}>
+        {/* <View style={{ flex: 0.2, flexDirection: "column" }}> */}
+          <Button
+            buttonStyle={styles.buttonBase}
+            title="Get Tips"
+            onPress={() => getTipsRangeDate('uid',global.userToken.uid)}
+            titleStyle={{ fontWeight: 'bold', fontSize: 18, color:'#000' }}
+          />
+        {/* </View> */}
           {/* 
           Add a button to run the query 
           */}
         </View>
         <ScrollView>
+          <FlatList
+          label="Tips History"
+          data={myTips}
+          renderItem={renderTipRecords}
+          keyExtractor={(item, index) => index}
+          style={{ flex: 1, margin: 20, height: 100 }}
+          />
           {/* 
           Create a new component called TipRecords that is the repeat of all your views
           {renderTipRecords}
